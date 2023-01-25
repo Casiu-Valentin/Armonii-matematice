94d8908794611e6d20004a0e9228d4822b523899
George Valentin Casiu <casrom@gmail.com>
2023-01-25 16:51:26 +0200

    Update citire_numar_natural_1.py

--- citire numar natural/citire_numar_natural_1.py
+++ citire numar natural/citire_numar_natural_1.py
@@ -1,8 +1,13 @@
-def citire():# citire numar natural
+# Realizat de /Produced by Casiu George Valentin
+"""
+Se citeşte n, număr natural/It reads n, natural number
+"""
+
+def citire():# citire numar natural /natural number reading
     n=''
-    while not(n.isdecimal()): # daca n nu e zecimal
-        n = input("Introduceti un numar natural:")
-        n = n.strip(' +')# elimina spatiile libere si '+' de la inceputul si sfarsitul numarului
+    while not(n.isdecimal()): # daca n nu e zecimal /if n is not decimal
+        n = input("Introduceti un numar natural /Enter a natural number:")
+        n = n.strip(' +')# elimina spatiile libere si '+' de la inceputul si sfarsitul numarului /remove spaces and '+' from the beginning and end of number
     return int(n)
 
 print(citire())
