a84df90984f2e96b9157da0b425654275c5faf84
George Valentin Casiu <casrom@gmail.com>
2023-01-25 16:55:05 +0200

    Update citire_numar_natural_2.py

--- citire numar natural/citire_numar_natural_2.py
+++ citire numar natural/citire_numar_natural_2.py
@@ -3,15 +3,15 @@
 Se citeşte n, număr natural/It reads n, natural number
 """
 
-def citire():# citire numar natural
+def citire():# citire numar natural/ Natural number reading
 	while True:
 		try:
-			n=int(input("Introduceti un numar natural:"))
+			n=int(input("Introduceti un numar natural/Enter a natural number:"))
 			if n>=0:
 				return n
 			else:
-				print("Numerele naturale sunt pozitive")
+				print("Numerele naturale sunt pozitive/Natural number a positive")
 		except ValueError:
-			print("Mai incearca")
+			print("Mai incearca/Try again")
 
 print(citire())
