9b166febeba6d3dd0b1a3d66d72b9ed92e82a406
George Valentin Casiu <casrom@gmail.com>
2023-01-28 08:00:44 +0200

    Update Expresie1.py

--- Calcul expresii matematice/Expresie1.py
+++ Calcul expresii matematice/Expresie1.py
@@ -5,14 +5,15 @@ E=E1+E2+.....+En
 unde/where Ei=1/1+1/1*2+1/1*2*3+......+1/1*2*...*i
 """
 
-def citire():# citire numar natural
+def citire(text):# citire numar natural /natural number reading
     n=''
-    while not(n.isdecimal()): # daca n nu e zecimal
-        n = input("Introduceti un numar natural:")
-        n = n.strip(' +')# elimina spatiile libere si '+' de la inceputul si sfarsitul numarului
+    while not(n.isdecimal()): # daca n nu e zecimal /if n is not decimal
+        n = input(text)
+        n = n.strip(' +')# elimina spatiile libere si '+' de la inceputul si sfarsitul numarului /remove spaces and '+' from the beginning and end of number
     return int(n)
 
-n=citire()#Programul principal/The main program
+
+n=citire("Introduceti un numar natural/Enter a natural number:")#Programul principal/The main program
 p=1
 s=0
 for i in range(1,n+1):
